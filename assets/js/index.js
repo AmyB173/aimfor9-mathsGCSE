b5f0d6ff1cb8dee04496bd5dc901bb6e06cab10a
AmyB173 <148871917+AmyB173@users.noreply.github.com>
2024-03-18 13:19:59 +0000

    question section structure in place with all buttons, no functionality yet

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,13 @@
+//global variables, could be changed by user using the back button
+
+let username; 
+let grade; 
+
+// This stores the name and grade of the student to use at the end.
 document.getElementById("submit").addEventListener("click", function() {
-    console.log("success");
-    console.log(document.querySelector('[name="name"]').value);
-    console.log(document.querySelector('[name="grade"]:checked').value);
-    document.getElementsByClassName("assessment-wrapper")[0].innerHTML = "";
+    username = document.querySelector('[name="name"]').value;
+    grade = document.querySelector('[name="grade"]:checked').value;
+
 });
+
+//This function creates the HTML structure for the question page
