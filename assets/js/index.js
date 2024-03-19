ada45c9c55e0bc70ab40350bcbfa000f98b5a9df
AmyB173 <148871917+AmyB173@users.noreply.github.com>
2024-03-19 05:32:29 +0000

    feat:add new js file for module content and will display first placeholder question

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,23 @@
+import {
+    module1
+} from './questions.js';
+
 //global variables, could be changed by user using the back button
 
 let username; 
-let grade; 
+let grade;
+console.log($())
 
 // Trigger for starting assessment
-document.getElementById("submit").addEventListener("click", startAssessment);
-
-// This stores the name and grade of the student to use at the end and starts the assessment
-function startAssessment() {
+document.getElementById("submit").addEventListener("click", () => {
     username = document.querySelector('[name="username"]').value;
     grade = document.querySelector('[name="grade"]:checked').value;
     $(".start-wrapper").addClass("d-none");
     $("#question-wrapper").removeClass("d-none");
+    insertFirstQuestion();
+});
+
+function insertFirstQuestion() {
+    $(".question-title")[0].innerHTML = module1[0].name;
+    $(".question-content")[0].innerHTML = module1[0].question;
 }
---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+
+export let module1Questions = [
+    {
+        name: "Expand single Brackets",
+        moduleCode: "1",
+        questionCode: "1",
+        question: `<p>Module 1 Topic 1 Question</p>`, 
+        answer: `<p>Module 1 Topic 1 Answer</p>`,
+        solution: `<img src="assets/images/logo.png"></img>`
+    },
+    {
+        name: "Expand Two Brackets",
+        moduleCode: "1",
+        questionCode: "2",
+        question: `<p>Module 1 Topic 2 Question</p>`, 
+        answer: `<p>Module 1 Topic 2 Answer</p>`,
+        solution: `<img src="assets/images/logo.png"></img>`
+    },
+    {
+        name: "Expand Three Brackets",
+        moduleCode: "1",
+        questionCode: "3",
+        question: `<p>Module 1 Topic 3 Question</p>`, 
+        answer: `<p>Module 1 Topic 3 Answer</p>`,
+        solution: `<img src="assets/images/logo.png"></img>`
+    }
+
+]
