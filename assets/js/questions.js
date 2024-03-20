153b79342d266181abe5bc24156c1397a94d98bb
AmyB173 <148871917+AmyB173@users.noreply.github.com>
2024-03-20 10:38:24 +0000

    feat: image of full solution added into question content - one added for test

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,29 +1,73 @@
-
+// the question and answer values contain long mathml code, collapse questions and answers for 
+// better readability
 export let modules = [
     {
     name: "module1",
+    displayName: "Expanding Brackets",
     content: [
     {
-        name: "Expand single Brackets",
+        name: "Expand single brackets with complex powers",
         moduleCode: 1,
         questionCode: 1,
         question: `<math display='block'>
         <semantics>
          <mrow>
-          <mn>2</mn><mo stretchy='false'>(</mo><mi>x</mi><mo>&#x2212;</mo><mn>1</mn><mo stretchy='false'>)</mo></mrow>
+          <mn>2</mn><msup>
+           <mi>x</mi>
+           <mn>2</mn>
+          </msup>
+          <mi>y</mi><mi>z</mi><mo stretchy='false'>(</mo><mn>4</mn><mi>x</mi><msup>
+           <mi>y</mi>
+           <mn>5</mn>
+          </msup>
+          <mo>&#x2212;</mo><mi>z</mi><mo stretchy='false'>)</mo></mrow>
          <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
          feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
          hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
          4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
          vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
          fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiecaaIYa
-         GaaiikaiaadIhacqGHsislcaaIXaGaaiykaaaa@3B50@
+         GaamiEamaaCaaaleqabaGaaGOmaaaakiaadMhacaWG6bGaaiikaiaa
+         isdacaWG4bGaamyEamaaCaaaleqabaGaaGynaaaakiabgkHiTiaadQ
+         hacaGGPaaaaa@4233@
          </annotation>
         </semantics>
        </math>
        `, 
-        answer: `<p>Module 1 Topic 1 Answer</p>`,
-        solution: `<img src="assets/images/logo.png"></img>`,
+        answer: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mn>8</mn><msup>
+           <mi>x</mi>
+           <mn>3</mn>
+          </msup>
+          <msup>
+           <mi>y</mi>
+           <mn>6</mn>
+          </msup>
+          <mi>z</mi><mo>&#x2212;</mo><mn>2</mn><msup>
+           <mi>x</mi>
+           <mn>2</mn>
+          </msup>
+          <mi>y</mi><msup>
+           <mi>z</mi>
+           <mn>2</mn>
+          </msup>
+          </mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiecaaI4a
+         GaamiEamaaCaaaleqabaGaaG4maaaakiaadMhadaahaaWcbeqaaiaa
+         iAdaaaGccaWG6bGaeyOeI0IaaGOmaiaadIhadaahaaWcbeqaaiaaik
+         daaaGccaWG5bGaamOEamaaCaaaleqabaGaaGOmaaaaaaa@42BC@
+         </annotation>
+        </semantics>
+       </math>
+       `,
+        solution: `<img src="assets/images/solutions/11.webp">`,
         mastered: false
     },
     {
