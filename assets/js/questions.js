0f8c2423d5eaa66862d4c79763b6389038d7c846
AmyB173 <148871917+AmyB173@users.noreply.github.com>
2024-03-20 17:04:13 +0000

    feat: add content for module 3 to questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -167,7 +167,7 @@ export let modules = [
     displayName: "Factorising",
     content: [
     {
-        name: "Factorising",
+        name: "Factorising into single brackets with complex powers",
         moduleCode: 2,
         questionCode: 1,
         question: `<math display='block'>
@@ -223,7 +223,7 @@ export let modules = [
         mastered: false
     },
     {
-        name: "Factorising",
+        name: "Factorising a quadratic where to coefficient of x is 1",
         moduleCode: 2,
         questionCode: 2,
         question: `<math display='block'>
@@ -266,7 +266,7 @@ export let modules = [
         mastered: false
     },
     {
-        name: "Factorising",
+        name: "Difference of two squares factorising",
         moduleCode: 2,
         questionCode: 3,
         question: `<math display='block'>
@@ -308,7 +308,7 @@ export let modules = [
         mastered: false
     },
     {
-        name: "Solve complex equations",
+        name: "Factorising quadratics where the coefficent of x is greater than 1",
         moduleCode: 2,
         questionCode: 4,
         question: `<math display='block'>
@@ -351,7 +351,7 @@ export let modules = [
         mastered: false
     },
     {
-        name: "Factorising",
+        name: "Difference of two squares factorising where the coefficient of x is greater than 1",
         moduleCode: 2,
         questionCode: 5,
         question: `<math display='block'>
@@ -391,6 +391,291 @@ export let modules = [
        </math>
        `,
         solution: `<img src="assets/images/solutions/25.webp">`,
+        mastered: false
+    }]
+},
+{
+    name: "module3",
+    displayName: "Simplifying Algebraic Fractions",
+    content: [
+    {
+        name: "Simplifying simple fractions with letter and number cancellation",
+        moduleCode: 3,
+        questionCode: 1,
+        question: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mfrac>
+           <mrow>
+            <mn>12</mn><msup>
+             <mi>x</mi>
+             <mn>5</mn>
+            </msup>
+            <msup>
+             <mi>y</mi>
+             <mn>6</mn>
+            </msup>
+            </mrow>
+           <mrow>
+            <mn>3</mn><msup>
+             <mi>x</mi>
+             <mn>2</mn>
+            </msup>
+            <msup>
+             <mi>y</mi>
+             <mn>2</mn>
+            </msup>
+            <mi>z</mi></mrow>
+          </mfrac>
+          </mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiedaWcaa
+         qaaiaaigdacaaIYaGaamiEamaaCaaaleqabaGaaGynaaaakiaadMha
+         daahaaWcbeqaaiaaiAdaaaaakeaacaaIZaGaamiEamaaCaaaleqaba
+         GaaGOmaaaakiaadMhadaahaaWcbeqaaiaaikdaaaGccaWG6baaaaaa
+         @41A2@
+         </annotation>
+        </semantics>
+       </math>
+       
+       `, 
+        answer: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mfrac>
+           <mrow>
+            <mn>4</mn><msup>
+             <mi>x</mi>
+             <mn>3</mn>
+            </msup>
+            <msup>
+             <mi>y</mi>
+             <mn>4</mn>
+            </msup>
+            </mrow>
+           <mi>z</mi>
+          </mfrac>
+          </mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiedaWcaa
+         qaaiaaisdacaWG4bWaaWbaaSqabeaacaaIZaaaaOGaamyEamaaCaaa
+         leqabaGaaGinaaaaaOqaaiaadQhaaaaaaa@3C47@
+         </annotation>
+        </semantics>
+       </math>       
+       `,
+        solution: `<img src="assets/images/solutions/31.webp">`,
+        mastered: false
+    },
+    {
+        name: "Factorise numerator or denominator and cancel brackets",
+        moduleCode: 3,
+        questionCode: 2,
+        question: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mfrac>
+           <mrow>
+            <mn>2</mn><msup>
+             <mi>x</mi>
+             <mn>2</mn>
+            </msup>
+            <mo>&#x2212;</mo><mn>9</mn><mi>x</mi><mo>&#x2212;</mo><mn>5</mn></mrow>
+           <mrow>
+            <mi>x</mi><mo>&#x2212;</mo><mn>5</mn></mrow>
+          </mfrac>
+          </mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiedaWcaa
+         qaaiaaikdacaWG4bWaaWbaaSqabeaacaaIYaaaaOGaeyOeI0IaaGyo
+         aiaadIhacqGHsislcaaI1aaabaGaamiEaiabgkHiTiaaiwdaaaaaaa@4054@
+         </annotation>
+        </semantics>
+       </math>       
+       `, 
+        answer: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mn>2</mn><mi>x</mi><mo>+</mo><mn>1</mn></mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiecaaIYa
+         GaamiEaiabgUcaRiaaigdaaaa@39EC@
+         </annotation>
+        </semantics>
+       </math>
+       `,
+        solution: `<img src="assets/images/solutions/32.webp">`,
+        mastered: false
+    },
+    {
+        name: "Factorising and cancelling when a squared bracket is involved",
+        moduleCode: 3,
+        questionCode: 3,
+        question: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mfrac>
+           <mrow>
+            <msup>
+             <mi>x</mi>
+             <mn>2</mn>
+            </msup>
+            <mo>+</mo><mn>2</mn><mi>x</mi><mo>&#x2212;</mo><mn>15</mn></mrow>
+           <mrow>
+            <msup>
+             <mrow>
+              <mo stretchy='false'>(</mo><mi>x</mi><mo>&#x2212;</mo><mn>3</mn><mo stretchy='false'>)</mo></mrow>
+             <mn>2</mn>
+            </msup>
+            </mrow>
+          </mfrac>
+          </mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiedaWcaa
+         qaaiaadIhadaahaaWcbeqaaiaaikdaaaGccqGHRaWkcaaIYaGaamiE
+         aiabgkHiTiaaigdacaaI1aaabaGaaiikaiaadIhacqGHsislcaaIZa
+         GaaiykamaaCaaaleqabaGaaGOmaaaaaaaaaa@4281@
+         </annotation>
+        </semantics>
+       </math>
+       
+       `, 
+        answer: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mfrac>
+           <mrow>
+            <mi>x</mi><mo>+</mo><mn>5</mn></mrow>
+           <mrow>
+            <mi>x</mi><mo>&#x2212;</mo><mn>3</mn></mrow>
+          </mfrac>
+          </mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiedaWcaa
+         qaaiaadIhacqGHRaWkcaaI1aaabaGaamiEaiabgkHiTiaaiodaaaaa
+         aa@3BEB@
+         </annotation>
+        </semantics>
+       </math>       
+       `,
+        solution: `<img src="assets/images/solutions/33.webp">`,
+        mastered: false
+    },
+    {
+        name: "Factorising quadratics where the coefficent of x is greater than 1",
+        moduleCode: 3,
+        questionCode: 4,
+        question: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mfrac>
+           <mrow>
+            <mn>32</mn><msup>
+             <mi>x</mi>
+             <mn>2</mn>
+            </msup>
+            <mo>&#x2212;</mo><mn>50</mn></mrow>
+           <mrow>
+            <mn>4</mn><mi>x</mi><mo>&#x2212;</mo><mn>5</mn></mrow>
+          </mfrac>
+          </mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiedaWcaa
+         qaaiaaiodacaaIYaGaamiEamaaCaaaleqabaGaaGOmaaaakiabgkHi
+         TiaaiwdacaaIWaaabaGaaGinaiaadIhacqGHsislcaaI1aaaaaaa@3FDC@
+         </annotation>
+        </semantics>
+       </math>       
+       `, 
+        answer: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mn>2</mn><mo stretchy='false'>(</mo><mn>4</mn><mi>x</mi><mo>+</mo><mn>5</mn><mo stretchy='false'>)</mo></mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiecaaIYa
+         GaaiikaiaaisdacaWG4bGaey4kaSIaaGynaiaacMcaaaa@3C07@
+         </annotation>
+        </semantics>
+       </math>       
+       `,
+        solution: `<img src="assets/images/solutions/34.webp">`,
+        mastered: false
+    },
+    {
+        name: "Complex simplification (avoid cancelling summed brackets)",
+        moduleCode: 3,
+        questionCode: 5,
+        question: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mfrac>
+           <mrow>
+            <mn>2</mn><mo stretchy='false'>(</mo><mi>x</mi><mo>+</mo><mn>3</mn><mo stretchy='false'>)</mo><mo>&#x2212;</mo><mn>4</mn><mo stretchy='false'>(</mo><mi>x</mi><mo>&#x2212;</mo><mn>2</mn><mo stretchy='false'>)</mo></mrow>
+           <mrow>
+            <mn>4</mn><mo stretchy='false'>(</mo><mi>x</mi><mo>+</mo><mn>3</mn><mo stretchy='false'>)</mo><mo>&#x2212;</mo><mn>2</mn><mo stretchy='false'>(</mo><mn>3</mn><mi>x</mi><mo>&#x2212;</mo><mn>1</mn><mo stretchy='false'>)</mo></mrow>
+          </mfrac>
+          </mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiedaWcaa
+         qaaiaaikdacaGGOaGaamiEaiabgUcaRiaaiodacaGGPaGaeyOeI0Ia
+         aGinaiaacIcacaWG4bGaeyOeI0IaaGOmaiaacMcaaeaacaaI0aGaai
+         ikaiaadIhacqGHRaWkcaaIZaGaaiykaiabgkHiTiaaikdacaGGOaGa
+         aG4maiaadIhacqGHsislcaaIXaGaaiykaaaaaaa@4C18@
+         </annotation>
+        </semantics>
+       </math>
+       `, 
+        answer: `<math display='block'>
+        <semantics>
+         <mn>1</mn>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiecaaIXa
+         aaaa@3751@
+         </annotation>
+        </semantics>
+       </math>
+       `,
+        solution: `<img src="assets/images/solutions/35.webp">`,
         mastered: false
     }]
 }
