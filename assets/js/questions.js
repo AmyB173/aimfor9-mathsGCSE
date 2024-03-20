6106221557411063b8d5361350eafdabd982c95a
AmyB173 <148871917+AmyB173@users.noreply.github.com>
2024-03-20 09:24:49 +0000

    test: insterting an equation into a question module and checking if this appears correctly and can be styled

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -7,7 +7,21 @@ export let modules = [
         name: "Expand single Brackets",
         moduleCode: 1,
         questionCode: 1,
-        question: `<p>Module 1 Topic 1 Question</p>`, 
+        question: `<math display='block'>
+        <semantics>
+         <mrow>
+          <mn>2</mn><mo stretchy='false'>(</mo><mi>x</mi><mo>&#x2212;</mo><mn>1</mn><mo stretchy='false'>)</mo></mrow>
+         <annotation encoding='MathType-MTEF'>MathType@MTEF@5@5@+=
+         feaahGart1ev3aaatCvAUfeBSjuyZL2yd9gzLbvyNv2CaerbuLwBLn
+         hiov2DGi1BTfMBaeXatLxBI9gBaerbd9wDYLwzYbItLDharqqtubsr
+         4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8F4rqqrFfpeea0xe9Lq=Jc9
+         vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=x
+         fr=xb9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbiqaaWiecaaIYa
+         GaaiikaiaadIhacqGHsislcaaIXaGaaiykaaaa@3B50@
+         </annotation>
+        </semantics>
+       </math>
+       `, 
         answer: `<p>Module 1 Topic 1 Answer</p>`,
         solution: `<img src="assets/images/logo.png"></img>`,
         mastered: false
