60f9d7effbe628628a230d949d0576c26e255c09
AmyB173 <148871917+AmyB173@users.noreply.github.com>
2024-04-05 07:11:43 +0100

    refactor: updated coments and formatting the index.js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,10 +10,10 @@ let username;
 let grade;
 
 // ----------------------START SECTION----------------------------
+
 /* 
-    Insert Module Names to "check to see what modules will be in the test" button
-    Designed to be dynamic so that the list will automatically populate with 
-    any additional modules
+    Insert Module Names and number of questions to "check to see what modules will be in the test" button
+    Designed to be dynamic so that the list will automatically populate with any additional modules or questions
  */
 window.onload = () => {
     let list = "";
@@ -23,7 +23,7 @@ window.onload = () => {
     $("#moduleList")[0].innerHTML = "<ul>" + `${list}` + "</ul>";
 }
 
-// Trigger for starting assessment and inserting question 1
+// Trigger for starting assessment, removing the starting text and inserting question 1
 $("#start-assessment").on("click", () => {
     username = document.querySelector('[name="username"]').value;
     grade = document.querySelector('[name="grade"]:checked').value;
@@ -32,13 +32,15 @@ $("#start-assessment").on("click", () => {
     $("#question-wrapper").removeClass("d-none");
     $("#question-additional-buttons").removeClass("d-none")
     insertNextQuestion();
-
 });
 
 // ----------------------QUESTION SECTION----------------------------
-// When show answer is clicked question title is replaced with the answer 
-// and the question is replaced with the fully worked solution
-// question buttons are replaced with answer buttons
+
+/* 
+    When show answer is clicked, the question title is replaced with the answer 
+    and the question is replaced with the fully worked solution
+    The question buttons are replaced with answer buttons
+*/
 $("#show-answer").on("click", () => {
     let currentModule = modules.find(module => module.name === "module" + currentModuleCode.toString());
     let currentQuestion = currentModule.content.find(question => question.questionCode === currentQuestionCode);
@@ -51,31 +53,49 @@ $("#show-answer").on("click", () => {
     $("#answer-buttons").removeClass("d-none");
 })
 
+// The "yes" and "too easy" buttons both indicate a pass on that question
 $("#yes-btn, #easy-btn").on("click", () => {
     questionFinished("pass");
 })
 
+// The "no" and "too hard" buttons both indicate a fail on that question
 $("#no-btn, #hard-btn").on("click", () => {
     questionFinished("fail");
 })
 
+/*
+    Returns to the previous question in the module unless
+    you are on the first question of a new module in which case
+    it will take you to the last question on the previous module
+*/
 $("#previous-btn").on("click", () => {
     if (currentQuestionCode > 1) {
         currentQuestionCode -= 1;
     } else {
+        //goes back to previous module
         //makes the question number the last question of the previous module
         currentModuleCode -= 1;
         currentQuestionCode = currentModule().content.length;
     }
-    //If they got the previous question wrong, but want to go back and correct it we don't want to double
-    //count the wrong answer if they get it wrong again
+    /*
+        If they got the previous question wrong, but want to go back and correct it we don't want to double
+        count the wrong answer if they get it wrong again
+    */
     if (currentQuestion().mastered = false) { failureCounter -= 1 };
     insertNextQuestion();
 })
 
+/*
+    the restart assessment button will reload the page 
+    (there is a bootstrap modal in index.html that warns them beforehand)
+*/
 $(".restart-btn").on("click", () => {
     location.reload();
 })
+
+/*
+    Ends the test by setting the module code greater than the number of modules
+*/
 $("#end-btn").on("click", () => {
     $('#end-modal').modal('hide');
     currentModuleCode = modules.length + 1;
@@ -92,8 +112,10 @@ function currentQuestion() {
     return currentModule().content.find(question => question.questionCode === currentQuestionCode);
 }
 
-// changes back to question mode by adding in next question and removing answer buttons
-// and adding question buttons
+/*  
+    changes back to question mode by adding in next question 
+    and removing answer button and adding question buttons
+*/
 function insertNextQuestion() {
     // previous button to be removed on first question
     if (currentQuestionCode === 1 && currentModuleCode === 1) {
@@ -109,15 +131,18 @@ function insertNextQuestion() {
     $(".question-content")[0].innerHTML = currentQuestion().question;
 }
 
-// marks the current questions as mastered, updates question and module codes
-// checks if the test if finished
-// if it's not finished next question is initiated
+/*
+    marks the current questions as mastered, updates question and module codes
+    checks if the test if finished
+    if it's not finished next question is initiated
+*/
 function questionFinished(result) {
-    // mastered is automatically false
-    // if they pass the question is marked as mastered
-    // if incorrect the failure counter updates the number of failed questions in the module
+    /*
+        mastered is automatically false
+        if they pass the question is marked as mastered
+        if incorrect the failure counter updates the number of failed questions in the module
+    */
     result === "pass" ? markAsMastered() : failureCounter += 1;
-
     updateCodes();
     if (checkIfEndOfTest()) { return };
     insertNextQuestion();
@@ -128,15 +153,16 @@ function markAsMastered() {
     modules.find(module => module.name === "module" + currentModuleCode).content.find(question => question.questionCode === currentQuestionCode).mastered = true;
 }
 
-// this function checks whether the module code should be updated and then updates if so
+// checks whether the module code should be updated and then updates if so
 function updateCodes() {
     let endOfModule = currentQuestionCode == modules.find(module => module.name === "module" + currentModuleCode).content.length;
     let twoWrong = failureCounter === 2;
-
+    /*
+        If end of module, all they already got two answers wrong
+        we move onto the next module, questions restart at 1 for new module
+        Resets failure counter for new module
+    */
     if (endOfModule || twoWrong) {
-        // This means we move onto the next module
-        // Questions restarts at 1 for new module
-        // Resets failure counter for new module
         currentModuleCode += 1;
         currentQuestionCode = 1;
         failureCounter = 0;
@@ -146,8 +172,11 @@ function updateCodes() {
     }
 }
 
-//This function checks if we are at the end of the test
-//If so results section is presented 
+/*
+    This function checks if we are at the end of the test
+    If so results section is presented 
+    If not, a value of false is returned
+*/
 function checkIfEndOfTest() {
     let endOfTest = currentModuleCode > modules.length;
     if (endOfTest) {
@@ -169,19 +198,17 @@ function checkIfEndOfTest() {
 }
 
 // ----------------------RESULTS SECTION----------------------------
-
-
     
 //Build a table of results for each module
 function populateResultsTable() { 
     $("#results")[0].innerHTML = "";
-
     for (const module of modules) {
+        //Adds the name of the topic
         $("#results")[0].innerHTML += `<h2>${module.displayName}</h2>`
         let resultsTable = "";
+        //Adds the result of the topic in the same row on the table
         for (const question of module.content) {
             let mastered;
-
             question.mastered ? mastered = "Mastered" : mastered = "Needs revision";
             resultsTable += `
             <tr>
